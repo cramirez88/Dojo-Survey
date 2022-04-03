dfafe79f76a240febb3e00e2876b0f0eb23ee386
Christian Ramirez <christian.r0602@gmail.com>
2022-04-02 20:37:52 -0500

    adding routes for submission

--- server.py
+++ server.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect
+
 
 # create an instance of flask
 app = Flask(__name__)
+app.secret_key = 'mnbvcxz'
 
 # ROUTES
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comments'] = request.form['comments']
+    return redirect('/results')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
+
 
 
 if __name__ == '__main__':
